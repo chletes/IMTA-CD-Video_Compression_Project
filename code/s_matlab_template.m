047ada5d93e75b8192eb33fd29673b786919e25d
chletes <40454278+chletes@users.noreply.github.com>
2020-02-26 13:56:29 +0100

    Matlab templates uploaded.
    The idea is to use this templates to write any function or any script (scenario) so all the members can understand the code.

---
+++ code/s_matlab_template.m
@@ -0,0 +1,23 @@
+%
+%
+%
+% PURPOSE : 
+%
+% USAGE : (Command Window) :
+%
+%
+%
+% 
+% EXTERNAL FUNCTIONS USED :
+%
+%
+% 
+% REFERENCES/NOTES/COMMENTS :
+%
+%
+% 
+%**********************************************************************************************
+
+close all, clc, clear
+clc
+

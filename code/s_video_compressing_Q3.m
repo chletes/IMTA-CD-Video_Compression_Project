bd8831e2cba2908a63fa4b5d2da34ed6c3d520a3
minhIMT <61503934+minhIMT@users.noreply.github.com>
2020-03-18 14:21:06 +0100

    update predicted code in Q3

--- code/s_video_compressing_Q3.m
+++ code/s_video_compressing_Q3.m
@@ -22,29 +22,41 @@ clc, clear, close all;
 
 %Filename
 file = "../data/images/news.qcif";
-
+Nframe = 10;
 % Open the file
 fid = fopen(file,'r');
 if (fid == -1)
     disp('Error with your file, check the filename.');
 else
-    [compY,compU,compV]=yuv_import("../data/images/akiyo.qcif",[176,144],200,0);
-    compY_compression_video = cell(1,300);
-    compU_compression_video = cell(1,300);
-    compV_compression_video = cell(1,300);
-    compressed_infoY_video = cell(1,300);
-    compressed_infoU_video = cell(1,300);
-    compressed_infoV_video = cell(1,300);
-    compY_decoded_video = cell(1,300);
-    compU_decoded_video = cell(1,300);
-    compV_decoded_video = cell(1,300);
-    for i = 1:300
+    [compY,compU,compV]=yuv_import("../data/images/news.qcif",[176 144],Nframe,0);
+    compY_predict_video = cell(1,Nframe);
+    compU_predict_video = cell(1,Nframe);
+    compV_predict_video = cell(1,Nframe);    
+    compY_compression_video = cell(1,Nframe);
+    compU_compression_video = cell(1,Nframe);
+    compV_compression_video = cell(1,Nframe);
+    compressed_infoY_video = cell(1,Nframe);
+    compressed_infoU_video = cell(1,Nframe);
+    compressed_infoV_video = cell(1,Nframe);
+    compY_decoded_video = cell(1,Nframe);
+    compU_decoded_video = cell(1,Nframe);
+    compV_decoded_video = cell(1,Nframe);
+    %predicted coding
+    compY_predict_video{1} = compY{1};
+    compU_predict_video{1} = compU{1};
+    compV_predict_video{1} = compV{1};
+    for i = 2:Nframe
+        compY_predict_video{i} = compY{i} - compY{i-1};
+        compU_predict_video{i} = compU{i} - compU{i-1};
+        compV_predict_video{i} = compV{i} - compV{i-1};
+    end
+    for i = 1:Nframe
         size_compY = size (compY{i});
         size_compU = size (compU{i});
         size_compV = size (compV{i});
-        [compY_compression,compressed_infoY,QX] = f_jpeg_compression(compY{i});
-        [compU_compression,compressed_infoU,QX] = f_jpeg_compression(compU{i});
-        [compV_compression,compressed_infoV,QX] = f_jpeg_compression(compV{i});
+        [compY_compression,compressed_infoY,QX] = f_jpeg_compression(compY_predict_video{i});
+        [compU_compression,compressed_infoU,QX] = f_jpeg_compression(compU_predict_video{i});
+        [compV_compression,compressed_infoV,QX] = f_jpeg_compression(compV_predict_video{i});
         compY_compression_video{i} = compY_compression;
         compU_compression_video{i} = compU_compression;
         compV_compression_video{i} = compV_compression;
@@ -62,9 +74,9 @@ else
         compY_decoded_video{i} = compY_decoded;
         compU_decoded_video{i} = compU_decoded;
         compV_decoded_video{i} = compV_decoded;        
-        [compR, compG, compB] = f_yuv_to_rgb(compY{i}, compU{i}, compV{i});
+  %      [compR, compG, compB] = f_yuv_to_rgb(compY{i}, compU{i}, compV{i});
 
-        [compR_decoded, compG_decoded, compB_decoded] = f_yuv_to_rgb(compY_decoded, compU_decoded, compV_decoded);
+    %    [compR_decoded, compG_decoded, compB_decoded] = f_yuv_to_rgb(compY_decoded, compU_decoded, compV_decoded);
     %     rgbImage = cat(3, compR,compG,compB);
     %     rgbImage_decoded = cat(3, compR_decoded,compG_decoded,compB_decoded);
     %     gray_pixel = 0.27*compR + 0.67*compG + 0.06*compB;
@@ -73,12 +85,22 @@ else
     % Im(:,:,2)=compG_decoded;
     % Im(:,:,3)=compB_decoded;
     %     imshow(rgbImage);
-%          figure (2);
-%          subplot(2,1,1)
-%          imagesc(compY{1}); 
+%           figure (2);
+%           subplot(2,1,1)
+%           imagesc(compY{1}'); 
 %          subplot(2,1,2)
-%          imagesc(compY_decoded); 
+%           imagesc(compY_decoded'); 
     end
+    for i = 2:Nframe
+        compY_decoded_video{i} = compY_decoded_video{i} + compY_decoded_video{i-1};
+        compU_decoded_video{i} = compU_decoded_video{i} + compU_decoded_video{i-1};
+        compV_decoded_video{i} = compV_decoded_video{i} + compV_decoded_video{i-1};
+    end
+          figure (2);
+          subplot(2,1,1)
+          imagesc(compY{3}); 
+         subplot(2,1,2)
+          imagesc(compY_decoded_video{3}); 
     fclose(fid);
 end
 
